71aafdab75b68e9158cf9a161c7afd3b7b96a2cd
Okezika Anyanwu <redeemed610@gmail.com>
2025-05-09 15:56:31 -0500

    insertRec function completed, tests written

--- src/main/java/AppointmentBST.java
+++ src/main/java/AppointmentBST.java
@@ -12,6 +12,32 @@ public class AppointmentBST {
 
     private AppointmentNode insertRec(AppointmentNode root, PatientAppointment appointment) {
         // TODO: Implement logic to insert by appointment time (avoid duplicates)
+        AppointmentNode current = root;
+        while(current != null){
+
+            int currentTime = Integer.parseInt(current.data.appointmentTime.substring(0, 2) + current.data.appointmentTime.substring(3));
+            int appointmentTime = Integer.parseInt(appointment.appointmentTime.substring(0, 2) + appointment.appointmentTime.substring(3));
+
+            if (appointmentTime == currentTime){
+                return root;
+            }
+            else if (appointmentTime > currentTime){
+                if (current.right == null){
+                    current.right = new AppointmentNode(appointment);
+                    return root;
+                }
+                current = current.right;
+            }
+            else {
+                if (current.left == null){
+                    current.left = new AppointmentNode(appointment);
+                    return root;
+                }
+                current = current.left;
+            }
+        }
+
+        root = new AppointmentNode(appointment);
         return root;
     }
 
--- src/test/java/AppointmentBSTTest.java
+++ src/test/java/AppointmentBSTTest.java
@@ -10,4 +10,58 @@ public class AppointmentBSTTest {
         bst.addAppointment(p1);
         assertNotNull(bst.getRoot());
     }
+    @Test
+    public void testAddMultipleAppointments() {
+        AppointmentBST bst = new AppointmentBST();
+        PatientAppointment p1 = new PatientAppointment(1, "Test Patient 1", "09:00");
+        PatientAppointment p2 = new PatientAppointment(2, "Test Patient 2", "10:00");
+        PatientAppointment p3 = new PatientAppointment(3, "Test Patient 3", "08:00");
+        PatientAppointment p4 = new PatientAppointment(4, "Test Patient 4", "11:00");
+        PatientAppointment p5 = new PatientAppointment(5, "Test Patient 5", "07:00");
+
+        System.out.println("Patients created:");
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        System.out.println(p4);
+        System.out.println(p5);
+
+        bst.addAppointment(p1);
+        bst.addAppointment(p2);
+        bst.addAppointment(p3);
+        bst.addAppointment(p4);
+        bst.addAppointment(p5);
+
+        System.out.println("\nPatients in tree:");
+        assertNotNull(bst.getRoot());
+        bst.inOrderTraversal(bst.getRoot());
+    }
+
+    @Test
+    public void testAvoidDuplicateTimes() {
+        AppointmentBST bst = new AppointmentBST();
+        PatientAppointment p1 = new PatientAppointment(1, "Test Patient 1", "09:00");
+        PatientAppointment p2 = new PatientAppointment(2, "Test Patient 2", "09:00");
+        PatientAppointment p3 = new PatientAppointment(3, "Test Patient 3", "08:00");
+        PatientAppointment p4 = new PatientAppointment(4, "Test Patient 4", "08:00");
+        PatientAppointment p5 = new PatientAppointment(5, "Test Patient 5", "07:00");
+
+        System.out.println("\nPatients created:");
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        System.out.println(p4);
+        System.out.println(p5);
+
+        bst.addAppointment(p1);
+        bst.addAppointment(p2);
+        bst.addAppointment(p3);
+        bst.addAppointment(p4);
+        bst.addAppointment(p5);
+
+        System.out.println("\nPatients in tree:");
+        assertNotNull(bst.getRoot());
+        bst.inOrderTraversal(bst.getRoot());
+    }
+
 }
